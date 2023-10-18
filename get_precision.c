a2ce90f94d438da854756e63a79c219367b53a35
Julius Charles <cjmatheka@gmail.com>
2023-10-17 18:03:29 -0700

    Update get_precision file

--- get_precision.c
+++ get_precision.c
@@ -2,8 +2,8 @@
 
 /**
  * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
+ * @format: Formatted string for printing  the arguments
+ * @i: The list of arguments.
  * @list: list of arguments.
  *
  * Return: Precision.
